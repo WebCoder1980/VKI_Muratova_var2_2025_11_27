e21e01c155f653f1fb4eafb63ca0db61d0e6fd8d
Smagin Maxim Alexeevich <maxprogger@mail.ru>
2025-11-28 15:30:49 +0700

    Add sort & search

--- AppWindow/FishingProductsWindow.xaml.cs
+++ AppWindow/FishingProductsWindow.xaml.cs
@@ -40,12 +40,103 @@ namespace SmaginMA_2025_11_27.AppWindow
 
         private void Refresh()
         {
-            ItemsLB.ItemsSource = Db.AppProduct
+            if (Db == null)
+            {
+                return;
+            }
+
+            IQueryable<AppProductModel> query = Db.AppProduct
                 .Include(i => i.Manufacturer)
                 .Include(i => i.Supplier)
-                .Include(i => i.Category)
-                .Select(i => new AppProductUserControl(i))
+                .Include(i => i.Category);
+
+            string search = SearchTB.Text.ToLower();
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                query = query.Where(i =>
+                    i.Article.ToLower().Contains(search)
+                    || i.Name.ToLower().Contains(search)
+                    || i.Unit.ToLower().Contains(search)
+                    || i.Cost.ToString().ToLower().Contains(search)
+                    || i.MaxDiscount.ToString().ToLower().Contains(search)
+                    || i.Manufacturer.Name.ToLower().Contains(search)
+                    || i.Supplier.Name.ToLower().Contains(search)
+                    || i.Category.Name.ToLower().Contains(search)
+                    || i.CurrentDiscount.ToString().ToLower().Contains(search)
+                    || i.StockQuantity.ToString().ToLower().Contains(search)
+                    || i.Description.ToLower().Contains(search)
+                );
+            }
+
+            switch (((ComboBoxItem)SortPropertyCB.SelectedItem).Content)
+            {
+                case "Артикул":
+                    query = query.OrderBy(i => i.Article);
+                    break;
+
+                case "Название":
+                    query = query.OrderBy(i => i.Name);
+                    break;
+
+                case "Единицы измерения":
+                    query = query.OrderBy(i => i.Unit);
+                    break;
+
+                case "Стоимость":
+                    query = query.OrderBy(i => i.Cost);
+                    break;
+
+                case "Максимальная скидка":
+                    query = query.OrderBy(i => i.MaxDiscount);
+                    break;
+
+                case "Производитель":
+                    query = query.OrderBy(i => i.Manufacturer.Name);
+                    break;
+
+                case "Поставщик":
+                    query = query.OrderBy(i => i.Supplier.Name);
+                    break;
+
+                case "Категория":
+                    query = query.OrderBy(i => i.Category.Name);
+                    break;
+
+                case "Текущая скидка":
+                    query = query.OrderBy(i => i.CurrentDiscount);
+                    break;
+
+                case "Кол-во на складе":
+                    query = query.OrderBy(i => i.StockQuantity);
+                    break;
+
+                case "Описание":
+                    query = query.OrderBy(i => i.Description);
+                    break;
+
+                default:
+                    MessageBox.Show(SortPropertyCB.SelectedItem.ToString());
+                    break;
+            }
+
+            if ((string)((ComboBoxItem)SortOrderCB.SelectedItem).Content == "По убыванию")
+            {
+                query = query.Reverse();
+            }
+
+            ItemsLB.ItemsSource = query.Select(i => new AppProductUserControl(i))
                 .ToList();
+        }
+
+        private void Refresh(object sender, TextChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void Refresh(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
         }
     }
 }
